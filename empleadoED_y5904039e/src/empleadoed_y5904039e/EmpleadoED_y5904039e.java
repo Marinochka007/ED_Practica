4e45a87fc074ea95a195d2fc9ee32c107a50941f
Marinochka007 <663509729m@gmail.com>
2021-01-13 13:20:13 +0100

    version final con Javadoc NIE Y5904039E

--- empleadoED_y5904039e/src/empleadoed_y5904039e/EmpleadoED_y5904039e.java
+++ empleadoED_y5904039e/src/empleadoed_y5904039e/EmpleadoED_y5904039e.java
@@ -1,9 +1,9 @@
 package empleadoed_y5904039e;
+/*EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED
+@author Marina Smirnova
+la version 1.0*/
 import java.util.*;
-/**
- *
- * @author ASUS
- */
+
 public class EmpleadoED_y5904039e {
 static final double FACTOR_HORA_EXTRA = 1.2;
 private String nombreCompleto, dni;
@@ -12,19 +12,30 @@ public EmpleadoED_y5904039e(String nombreCompleto, String dni) {
 this.nombreCompleto = nombreCompleto;
 this.dni = dni;
 }
+/*Constructor con 3 parametros
+*@param cantidat nombreCompleto nombre completo en mayúsculas
+*@dni documento nacional de identidad con letra y sin espacios
+*@param salario
+*/
 public double getSalarioEsteMes(int horasExtra) {
+/*@param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+*/
 double cantidadExtra, salarioFinal;
 cantidadExtra = horasExtra * this.salarioBasePorHora *
 FACTOR_HORA_EXTRA;
 salarioFinal=this.salarioBasePorHora * 40 * 4 + cantidadExtra;
 salarioFinal=(double)Math.round(salarioFinal * 100d) / 100d;
 return salarioFinal;
+/*devuelve el salario bruto mensual en euros con dos decimales
+*/
 }
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        EmpleadoED_y5904039e emp = new EmpleadoED_y5904039e("Marina Smirnova","Y5904039e");
+/*Calcula el salario bruto mensual en función del salario base por hora y
+las horas extras.
+◦ El precio por hora de la hora extra se determina con la
+constante FACTOR_HORA_EXTRA
+*/
+      EmpleadoED_y5904039e emp = new EmpleadoED_y5904039e("Marina Smirnova","Y5904039E");
 int horasExtra = 2;
 System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra)+ " euros, al hacer " + horasExtra + " horas extra.");
 }
